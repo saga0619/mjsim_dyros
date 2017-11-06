bbb40004c5f8a9dcadb4317e02e882730cc1447c
안준휘_lab <june992@seoul.ac.kr>
2017-11-06 14:54:29 +0900

    v2

--- mjsim/mjsim/mjsim.cpp
+++ mjsim/mjsim/mjsim.cpp
@@ -6,6 +6,9 @@
 
 int main()
 {
+
+
+	
     return 0;
 }
 
--- mjsim/mjsim/stdafx.h
+++ mjsim/mjsim/stdafx.h
@@ -7,9 +7,17 @@
 
 #include "targetver.h"
 
+
 #include <stdio.h>
 #include <tchar.h>
 
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+
+#include <string.h>
+#include <iostream>
+#include <string.h>
+#include <Eigen\dense>
+#include <mujoco.h>
+#include <glfw3.h>
